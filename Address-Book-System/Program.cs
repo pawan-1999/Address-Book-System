c52219dabd8b962a62ab5dcd8bdc5ca34570fee7
pawan-1999 <guptapawan767@gmail.com>
2021-08-03 20:50:35 +0530

    Added : Main Method

--- Address-Book-System/Program.cs
+++ Address-Book-System/Program.cs
@@ -9,17 +9,18 @@ namespace Address_Book_System
             Console.WriteLine("Welcome to Address book program");
 
             AddressBook.addAddressBook();
-            Console.WriteLine("\n");
-            AddressBook.ContactsDisplay();
-            Console.WriteLine("\n");
-            AddressBook.EditContact();
-            Console.WriteLine("\n");
-            AddressBook.ContactsDisplay();
-            Console.WriteLine("\n");
-            AddressBook.ContactsDisplay();
-            AddressBook.ContactsDisplay();
-            AddressBook.ContactsDisplay();
-            AddressBook.ContactsDisplay();
+            AddressBook.PersonSearch();
+            //Console.WriteLine("\n");
+            //AddressBook.ContactsDisplay();
+            //Console.WriteLine("\n");
+            //AddressBook.EditContact();
+            //Console.WriteLine("\n");
+            //AddressBook.ContactsDisplay();
+            //Console.WriteLine("\n");
+            //AddressBook.ContactsDisplay();
+            //AddressBook.ContactsDisplay();
+            //AddressBook.ContactsDisplay();
+            //AddressBook.ContactsDisplay();
 
         }
     }
