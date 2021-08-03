b9491b2d4e07d8dd918cdf578ff085fdc83426f3
pawan-1999 <guptapawan767@gmail.com>
2021-08-03 20:17:57 +0530

    Added : Creating Contact For Person

---
+++ Address-Book-System/AddressBook.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_Book_System
+{
+    class AddressBook
+    {
+        Contacts person = new Contacts();
+    }
+}
