eaadef2aaf56aea88d3b35cae5e11b72489de62a
pawan-1999 <guptapawan767@gmail.com>
2021-08-03 20:36:34 +0530

    Added : Main Method

--- Address-Book-System/Program.cs
+++ Address-Book-System/Program.cs
@@ -6,7 +6,7 @@ namespace Address_Book_System
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book System");
+            Console.WriteLine("Welcome to Address book program");
 
             AddressBook.AddContact();
             Console.WriteLine("\n");
